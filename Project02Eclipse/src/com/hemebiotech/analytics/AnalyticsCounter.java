e2c4830f2d9b25d3280fb869987f4463c72949c1
jonathan Gouveia <jonathan.gouveia@protonmail.com>
2020-10-20 17:39:32 +0200

    work in progress

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,25 +1,53 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+//import java.io.FileWriter;
+import java.util.List;
 
 public class AnalyticsCounter {
-	
+
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
+	private static int dialatedpupilCount = 0;
+	private static int unknown =0;
+	private static int constrictedpupils =0;
+
 	private static String symptomsFile = "../Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt";
-	
+
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader(symptomsFile));
-		String line = reader.readLine();
 
+		ReadSymptomDataFromFile read = new ReadSymptomDataFromFile(symptomsFile);
+		List<String> result = read.GetSymptoms();
+
+		for (byte i = 0; i < result.size(); i++) {
+			switch (result.get(i)) {
+			case "headache":
+				headacheCount++;
+				break;
+			case "dialated pupils":
+				dialatedpupilCount++;
+				break;
+			case "constricted pupils":
+				constrictedpupils++;
+				break;
+			case "rash":
+				rashCount++;
+				break;
+			default: unknown++;
+				break;
+			}
+			int pupils = constrictedpupils + dialatedpupilCount;
+			System.out.println(result.get(i));
+			System.out.println("number of headaches: " + headacheCount);
+			System.out.println("number of dialated pupils: " + dialatedpupilCount);
+			System.out.println("number of rash: " + rashCount);
+			System.out.println("number of unknown: " + unknown);
+			System.out.println("number of constricted pupils: " + constrictedpupils);
+			System.out.println("number of pupils: " + pupils);
+		}
+		/*
 		while (line != null) {
 			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
 				headacheCount++;
 				System.out.println("number of headaches: " + headacheCount);
 			}
@@ -33,11 +61,13 @@ public class AnalyticsCounter {
 		}
 		reader.close();
 		
+		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
+		*/
 	}
 }
