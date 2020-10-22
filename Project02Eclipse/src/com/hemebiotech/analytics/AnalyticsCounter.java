ec0aa396aed06cf31c7ed8560ef975d54594c687
jonathan Gouveia <jonathan.gouveia@protonmail.com>
2020-10-22 12:40:04 +0200

    outsourcing_of_the_write_method_in_WriteSymptomsResultInFile

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,124 +1,17 @@
 package com.hemebiotech.analytics;
 
-import java.io.FileWriter;
-import java.util.ArrayList;
 import java.util.List;
 
 public class AnalyticsCounter {
 
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int dialatedpupilCount = 0;
-	private static int unknown =0;
-	private static int constrictedpupils =0;
-
 	private static String symptomsFile = "../Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt";
 
 	public static void main(String args[]) throws Exception {
-		// first get input
 
 		ReadSymptomDataFromFile read = new ReadSymptomDataFromFile(symptomsFile);
 		List<String> result = read.GetSymptoms();
 		
-		/*
-
-		for (byte i = 0; i < result.size(); i++) {
-			switch (result.get(i)) {
-			case "headache":
-				headacheCount++;
-				break;
-			case "dialated pupils":
-				dialatedpupilCount++;
-				break;
-			case "constricted pupils":
-				constrictedpupils++;
-				break;
-			case "rash":
-				rashCount++;
-				break;
-			default: unknown++;
-				break;
-			}
-			int pupils = constrictedpupils + dialatedpupilCount;
-			System.out.println(result.get(i));
-			System.out.println("number of headaches: " + headacheCount);
-			System.out.println("number of dialated pupils: " + dialatedpupilCount);
-			System.out.println("number of rash: " + rashCount);
-			System.out.println("number of unknown: " + unknown);
-			System.out.println("number of constricted pupils: " + constrictedpupils);
-			System.out.println("number of pupils: " + pupils);
-		}*/
-		
-		FileWriter writerTest = new FileWriter ("result_test.out");
-		ArrayList<String> deduplication = new ArrayList<String>();
-		
-		deduplication.add("Prout"); //TO DO delete
-		System.out.println(deduplication); //TO DO delete
-		System.out.println(result); //TO DO delete
-		
-		
-		for (int i =0; i< result.size(); i++) { // browse all the table
-			
-			boolean exist = false;
-			int nombre =0;
-			String symptomReading = result.get(i);
-			
-			for(int j = 0; j<deduplication.size() && exist == false; j++) { //for each iteration, i look if the data exist in deduplication table
-				
-			String symptomAlreadyCount = deduplication.get(j);
-			
-				if(symptomReading.equals(symptomAlreadyCount)) {
-					exist = true;
-				}
-			}
-			if (exist == false) {
-				for (int k=0; k <result.size(); k++) {
-					
-					String compare =result.get(k);
-					if (symptomReading.equals(compare)) {
-						nombre += 1;
-					}
-				}
-				deduplication.add(symptomReading);
-				writerTest.write("number of " + symptomReading + " is " + nombre+ "\n");
-			}
-		}
-		System.out.println(deduplication); //TO DO delete
-		writerTest.close();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-			line = reader.readLine();	// get another symptom
-		}
-		reader.close();
-		
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-		*/
+		WriteSymptomsResultInFile write = new WriteSymptomsResultInFile(result);
+		write.WriteSymptoms();
 	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomsResultInFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomsResultInFile.java
@@ -1,6 +1,47 @@
 package com.hemebiotech.analytics;
 
-public class WriteSymptomsResultInFile {
-	
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class WriteSymptomsResultInFile {
+	private List<String> arraySymptoms;
+
+	public WriteSymptomsResultInFile(List<String> arraySymptoms) {
+		this.arraySymptoms = arraySymptoms;
+	}
+
+	public void WriteSymptoms() throws IOException {
+		FileWriter writerTest = new FileWriter("result_test.out");
+		ArrayList<String> deduplication = new ArrayList<String>();
+
+		for (int i = 0; i < arraySymptoms.size(); i++) { // browse all the table
+
+			boolean exist = false;
+			int nombre = 0;
+			String symptomReading = arraySymptoms.get(i);
+
+			for (int j = 0; j < deduplication.size() && exist == false; j++) { // for each iteration, i look if the data exist in deduplication table
+
+				String symptomAlreadyCount = deduplication.get(j);
+
+				if (symptomReading.equals(symptomAlreadyCount)) {
+					exist = true;
+				}
+			}
+			if (exist == false) {
+				for (int k = 0; k < arraySymptoms.size(); k++) {
+
+					String compare = arraySymptoms.get(k);
+					if (symptomReading.equals(compare)) {
+						nombre += 1;
+					}
+				}
+				deduplication.add(symptomReading);
+				writerTest.write("number of " + symptomReading + " is " + nombre + "\n");
+			}
+		}
+		writerTest.close();
+	}
 }
