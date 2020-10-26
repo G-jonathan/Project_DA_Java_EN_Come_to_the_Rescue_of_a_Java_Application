4cdaf532d372ab12ea67b178eed2977f3896c4d8
Jonathan Gouveia <jonathan.gouveia@protonmail.com>
2020-10-26 19:32:07 +0100

    move_FileWriter_declaration_as_a_class_param_and_inject_it_on_the_constructor+javadoc_and_comments_modifs

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,17 +5,19 @@ import java.util.List;
 public class AnalyticsCounter {
 
 	private static String symptomsFile = "../Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt";
+
 	/**
 	 * This is the entry point of the program
+	 * 
 	 * @param args
-	 * @throws Exception
+	 * @throws Exception If an input/output error occurs
 	 */
 	public static void main(String args[]) throws Exception {
 
 		ReadSymptomDataFromFile read = new ReadSymptomDataFromFile(symptomsFile);
 		List<String> result = read.GetSymptoms();
-		
-		WriteSymptomsResultInFile write = new WriteSymptomsResultInFile(result);
+
+		WriteSymptomsResultInFile write = new WriteSymptomsResultInFile(result, "result.out");
 		write.WriteSymptoms();
 	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomsResultInFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomsResultInFile.java
@@ -6,56 +6,69 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class contains a method which browse a list of symptoms,
- * counts the occurrences of each symptom and write the result to a file
+ * This class contains a method which browse a list of symptoms, counts the
+ * occurrences of each symptom and write the result to a file
+ * 
  * @author jonathan.G
  * @since 1.0
  */
-public class WriteSymptomsResultInFile implements ISymptomsWriter{
-	
+public class WriteSymptomsResultInFile implements ISymptomsWriter {
+
 	private List<String> arraySymptoms;
-	
+	private FileWriter writer;
+
 	/**
 	 * Constructor
+	 * 
 	 * @param arraySymptoms A list of symptoms
+	 * @param fileName      The name of the file that will contain the final data
+	 * @throws IOException If the named file exists but is a directory rather than a
+	 *                     regular file, does not exist but cannot be created, or
+	 *                     cannot be opened for any other reason
 	 */
-	public WriteSymptomsResultInFile(List<String> arraySymptoms) {
+	public WriteSymptomsResultInFile(List<String> arraySymptoms, String fileName) throws IOException {
 		this.arraySymptoms = arraySymptoms;
+		this.writer = new FileWriter(fileName);
 	}
-	
+
 	/**
-	 * Extract the differents symptoms and count their occurrences
-	 * Generates a file containing the list of symptoms and the number of times they appear in the file
-	 * @throws IOException 
+	 * Extract the different symptoms and count their occurrences
+	 * 
+	 * Each symptom already counted is added to an array which is checked at each
+	 * iteration to avoid counting the same symptom several times. Generates a file
+	 * containing the list of symptoms and the number of times they appear in the
+	 * file
+	 * 
+	 * @throws IOException If an input/output error occurs like a problem with a
+	 *                     file, drive or disk access type action
 	 */
 	public void WriteSymptoms() throws IOException {
-		FileWriter writer = new FileWriter("result.out");
 		ArrayList<String> deduplication = new ArrayList<String>();
 
-		for (int i = 0; i < arraySymptoms.size(); i++) { // browse all the arraySymptoms array
+		for (int i = 0; i < arraySymptoms.size(); i++) {
 
 			boolean exist = false;
 			int nombre = 0;
 			String symptomReading = arraySymptoms.get(i);
 
-			for (int j = 0; j < deduplication.size() && exist == false; j++) { // for each iteration, we look if the data exist in deduplication array
+			for (int j = 0; j < deduplication.size() && exist == false; j++) {
 
 				String symptomAlreadyCount = deduplication.get(j);
 
-				if (symptomReading.equals(symptomAlreadyCount)) { // if the data already exist in deduplication array
+				if (symptomReading.equals(symptomAlreadyCount)) {
 					exist = true;
 				}
 			}
-			if (exist == false) { // if the data doesn't exist in deduplication array
-				for (int k = 0; k < arraySymptoms.size(); k++) { // we browse the arraySymptoms array to count the number of occurrences
+			if (exist == false) {
+				for (int k = 0; k < arraySymptoms.size(); k++) {
 
-					String compare = arraySymptoms.get(k); 
+					String compare = arraySymptoms.get(k);
 					if (symptomReading.equals(compare)) {
 						nombre += 1;
 					}
 				}
-				writer.write("The symptom: \"" + symptomReading + "\" appears " + nombre + " times." + "\n"); // we write the symptom and its number in the file
-				deduplication.add(symptomReading); // then we add this data in deduplication array to no longer have to count it	
+				writer.write("The symptom: \"" + symptomReading + "\" appears " + nombre + " times." + "\n");
+				deduplication.add(symptomReading);
 			}
 		}
 		writer.close();
