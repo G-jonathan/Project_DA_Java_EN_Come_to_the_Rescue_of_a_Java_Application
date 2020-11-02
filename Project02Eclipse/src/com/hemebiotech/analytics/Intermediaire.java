fdb0b250298c55028aa5d8c6bc9f4a4e5674570b
Jonathan Gouveia <jonathan.gouveia@protonmail.com>
2020-11-02 17:41:33 +0100

    Write_the_readme_and_modify_the_file_organization

--- Project02Eclipse/src/com/hemebiotech/analytics/Intermediaire.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Intermediaire.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Intermediaire {
 	
 	private static String symptomsFile = "../Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt";
-	private static String resultfile = "result.out";
+	private static String resultfile = "../Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/result.out";
 	private Proceed go;
 	
 	public Intermediaire(){
